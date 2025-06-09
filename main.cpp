60c39fe15d6534c1cde21cc398465c3c16181dfd
main <main>
2025-06-09 22:59:11 +0300

    Clean up and reindex files

--- main.cpp
+++ main.cpp
@@ -5,8 +5,20 @@ int main(int argc, char *argv[])
 {
 // For example
 
-    YeelightLib::Bulb bulb("192.168.1.49","55443");
-    printf("Connection: %s",bulb.TestConnection());
+    try {
+        YeelightLib::Bulb bulb("192.168.1.49", "55443");
+        printf("Connection: %s", bulb.TestConnection());
+
+        if (bulb.TestConnection()) {
+            bulb.SetPower(true, "smooth", 1500);
+        } else {
+            printf("Error: No connection to the bulb\n");
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Exception occurred: " << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Unknown exception occurred" << std::endl;
+    }
     QCoreApplication a(argc, argv);
 
     return a.exec();
